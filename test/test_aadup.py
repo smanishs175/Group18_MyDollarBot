403bf55b6ca34389c0f18d4bbdf418508dce22f9
Manish Shinde <smanishs175@gmail.com>
2022-09-29 14:45:41 -0400

    Update addup tests

--- test/test_aadup.py
+++ test/test_aadup.py
@@ -59,16 +59,16 @@ def test_post_amount_input_working_withdata(mock_telebot, mocker):
     mc = mock_telebot.return_value
     mc.send_message.return_value = True
     mocker.patch.object(add, 'helper')
-    add.helper.validate_entered_amount.return_value = 10
-    add.helper.write_json.return_value = True
-    add.helper.getDateFormat.return_value = dateFormat
-    add.helper.getTimeFormat.return_value = timeFormat
+    addup.helper.validate_entered_amount.return_value = 10
+    addup.helper.write_json.return_value = True
+    addup.helper.getDateFormat.return_value = dateFormat
+    addup.helper.getTimeFormat.return_value = timeFormat
 
     mocker.patch.object(add, 'option')
-    add.option.return_value = {11, "here"}
+    addup.option.return_value = {11, "here"}
 
     message = create_message("hello from testing!")
-    add.post_amount_input(message, mc, 'Food')
+    addup.post_amount_input(message, mc, 'Food')
     assert(mc.send_message.called)
 
 
@@ -78,9 +78,9 @@ def test_post_amount_input_nonworking(mock_telebot, mocker):
     mc.send_message.return_value = True
     mc.reply_to.return_value = True
     mocker.patch.object(add, 'helper')
-    add.helper.validate_entered_amount.return_value = 0
+    addup.helper.validate_entered_amount.return_value = 0
     message = create_message("hello from testing!")
-    add.post_amount_input(message, mc, 'Food')
+    addup.post_amount_input(message, mc, 'Food')
     assert(mc.reply_to.called)
 
 
@@ -89,26 +89,26 @@ def test_post_amount_input_working_withdata_chatid(mock_telebot, mocker):
     mc = mock_telebot.return_value
     mc.send_message.return_value = True
     mocker.patch.object(add, 'helper')
-    add.helper.validate_entered_amount.return_value = 10
-    add.helper.write_json.return_value = True
-    add.helper.getDateFormat.return_value = dateFormat
-    add.helper.getTimeFormat.return_value = timeFormat
+    addup.helper.validate_entered_amount.return_value = 10
+    addup.helper.write_json.return_value = True
+    addup.helper.getDateFormat.return_value = dateFormat
+    addup.helper.getTimeFormat.return_value = timeFormat
 
     mocker.patch.object(add, 'option')
-    add.option = {11, "here"}
+    addup.option = {11, "here"}
     test_option = {}
     test_option[11] = "here"
-    add.option = test_option
+    addup.option = test_option
 
     message = create_message("hello from testing!")
-    add.post_amount_input(message, mc, 'Food')
+    addup.post_amount_input(message, mc, 'Food')
     assert(mc.send_message.called)
     assert(mc.send_message.called_with(11, ANY))
 
 
 def test_add_user_record_nonworking(mocker):
     mocker.patch.object(add, 'helper')
-    add.helper.read_json.return_value = {}
+    addup.helper.read_json.return_value = {}
     addeduserrecord = add.add_user_record(1, "record : test")
     assert(addeduserrecord)
 
@@ -116,7 +116,7 @@ def test_add_user_record_nonworking(mocker):
 def test_add_user_record_working(mocker):
     MOCK_USER_DATA = test_read_json()
     mocker.patch.object(add, 'helper')
-    add.helper.read_json.return_value = MOCK_USER_DATA
+    addup.helper.read_json.return_value = MOCK_USER_DATA
     addeduserrecord = add.add_user_record(1, "record : test")
     if(len(MOCK_USER_DATA) + 1 == len(addeduserrecord)):
         assert True
