a54bbc7e2b0695fc46a7f76c1666327577564b99
Manish Shinde <smanishs175@gmail.com>
2022-09-29 15:36:03 -0400

    Update tests

--- test/test_start_and_menu_command.py
+++ test/test_start_and_menu_command.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
-import pytest
-#from code.code import start_and_menu_command
+# import pytest
+# from code.code import start_and_menu_command
+
 
 def test_start_and_menu_command_func():
-    #test_result = start_and_menu_command("/start")
-    #assert True == test_result, "Normal Case"
+    # test_result = start_and_menu_command("/start")
+    # assert True == test_result, "Normal Case"
     print("Hello")
