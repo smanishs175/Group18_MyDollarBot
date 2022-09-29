9156c378f7600120df00331e750725be9c5c1e66
Manish Shinde <smanishs175@gmail.com>
2022-09-29 15:52:42 -0400

    Updated and added run.sh, user.properties, contriburing.md, travis.yml

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='Track My Dollar',
+    name='Group 18_Track My Dollar_V2',
     version='1.0',
     description='An easy to use Telegram Bot to track everyday expenses',
-    author='Dev, Prakruthi, Radhika, Rohan, Sunidhi',
+    author='Nihar , Saksham , Manish, Palash,Shruti',
     scripts=['code/code.py'],
     packages=find_packages()
 )   
