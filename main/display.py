9f389196c79ac0764ce73105086cd05035e600fb
Shruti Verma~ <sverma5@ncsu.edu>
2022-10-05 18:21:42 -0400

    Updated all expenses and shared expenses methods

--- main/display.py
+++ main/display.py
@@ -8,7 +8,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from datetime import datetime
 from telebot import types
 import helper
-
+import os
 
 def run(message,bot):
     print("run display")
@@ -71,33 +71,27 @@ def show_categories(message,bot):
         bot.reply_to(message, str(e))
 
 
-def expense_category(message,bot):
+def expense_category(message,bot,start_date,end_date):
+    try:
+        chat_id = message.chat.id
+        choice_category = message.text
+        if not choice_category in helper.spend_categories:
+            raise Exception("Sorry I can't show spendings for \"{}\"!".format(choice_category))
 
-    #try
+        plots.categorical_plot(str(chat_id),message, start_date, end_date, choice_category)
+        print("executed")
+        bot.send_photo(chat_id, photo=open('categorical_expenses.png', 'rb'))
+        os.remove('categorical_expenses.png')
 
-    chat_id = message.chat.id
-    choice_category = message.text
-    if not choice_category in helper.spend_categories:
-        raise Exception("Sorry I can't show spendings for \"{}\"!".format(choice_category))
-
-    start_date=helper.date_range[0]
-    end_date=helper.date_range[1]
-    print(start_date)
-    print(end_date)
-    print(choice_category)
-
-    plots.categorical_plot(str(chat_id),message, start_date, end_date, choice_category)
-    print("executed")
-    bot.send_photo(chat_id, photo=open('categorical_expenses.png', 'rb'))
-    os.remove('categorical_expenses.png')
-
-    #except Exception as e:
-        #bot.reply_to(message, str(e))
+    except Exception as e:
+        bot.reply_to(message, str(e))
 
 def display_total(message,bot):
     try:
         chat_id = message.chat.id
         choice = message.text
+        start_date = helper.date_range[0]
+        end_date = helper.date_range[1]
 
         if not choice in helper.spend_display_option:
             raise Exception("Sorry I can't show spendings for \"{}\"!".format(choice))
@@ -113,9 +107,9 @@ def display_total(message,bot):
         total_text = ""
 
         if choice == 'All Expenses':
-            print(choice)
-        #     query = datetime.now().today().strftime(dateFormat)
-        #     queryResult = [value for index, value in enumerate(history) if str(query) in value] #query all that contains today's date
+            plots.overall_plot(str(chat_id), start_date, end_date)
+            bot.send_photo(chat_id, photo=open('overall_expenses.png', 'rb'))
+            os.remove('overall_expenses.png')
 
         elif choice == 'Category Wise':
             helper.read_json()
@@ -126,21 +120,15 @@ def display_total(message,bot):
             for c in helper.spend_categories:
                 markup.add(c)
             msg = bot.reply_to(message, 'Select Category', reply_markup=markup)
-            bot.register_next_step_handler(msg, expense_category,bot)
+            bot.register_next_step_handler(msg, expense_category,bot,start_date,end_date)
 
-            # query = datetime.now().today().strftime(monthFormat)
-            # queryResult = [value for index, value in enumerate(history) if str(query) in value] #query all that contains today's date
 
-        # total_text = calculate_spendings(queryResult)
-        # spending_text = ""
         elif choice=='Shared Expense':
-            print(choice)
-        # if len(total_text) == 0:
-        #     spending_text = "You have no spendings for {}!".format(DayWeekMonth)
-        # else:
-        #     spending_text = "Here are your total spendings {}:\nCATEGORIES,AMOUNT \n----------------------\n{}".format(DayWeekMonth.lower(), total_text)
+            plots.owe(str(chat_id))
+            bot.send_photo(chat_id, photo=open('owe.png', 'rb'))
+            os.remove('owe.png')
 
-        # bot.send_message(chat_id, choice)
+
     except Exception as e:
         bot.reply_to(message, str(e))
 
