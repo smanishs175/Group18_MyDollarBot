b88674984754cf1b2cbad6e119676e1323c1dfdf
Palash <palash10129@gmail.com>
2022-10-07 00:04:47 -0400

    Added functionality for plotting what I owe (shared expenses)

--- main/plots.py
+++ main/plots.py
@@ -131,3 +131,50 @@ def categorical_plot(chat_id, start_date, end_date,selected_cat):
             plt.savefig('categorical_expenses.png', bbox_inches='tight')
             helper.date_range=[]
             return 7
+
+
+def owe(chat_id):
+    check_data_val = check_data_present(chat_id) 
+    if check_data_val == 1:
+        return 1
+    elif check_data_val == 2:
+        return 2 
+    else:
+        all_ids = []
+        for j in expense_dict[chat_id]['group_expenses']:
+            all_ids+= transaction_dict[j]['members'].keys()
+            all_ids = list(set(all_ids))
+            all_ids.remove(chat_id)
+        
+        owe_dict = {}
+        for m in all_ids:
+            owe_dict[m] = []    
+        for j in expense_dict[chat_id]['group_expenses']:
+            temp_dict = transaction_dict[j]
+            creator_id = temp_dict['created_by']
+            if chat_id == creator_id:
+                for c_id in temp_dict['members'].keys():
+                    if c_id != chat_id:
+                        owe_dict[c_id] = owe_dict[c_id] + [temp_dict['members'][c_id]]
+            else:
+                owe_dict[creator_id] = owe_dict[creator_id] + [-1 * (temp_dict['members'][chat_id] )]
+        
+        x,y= [] , [] 
+        for k in owe_dict.keys():
+            val = owe_dict[k] 
+            if val != []:
+                x.append(user_key[k])
+                y.append(sum(val)*-1)# what I owe will be positive on plot
+    
+        rand_val = np.random.randint(10001,15000)
+        plt.figure(rand_val)            
+        plt.title("What I owe")
+        plt.ylabel('Amount ($)')
+        plt.xlabel('User')
+        #plt.xticks(rotation=45)
+        label_amount(y)
+        plt.bar(x,y,color=['r', 'g', 'b', 'y', 'm', 'c', 'k'])
+        plt.savefig('owe.png', bbox_inches='tight')
+        return 7
+    
+
